54767fcb303ab9feee1e042d598b0f279f7f6a49
Oloir <swetozarka@yandex.ru>
2020-05-29 03:02:25 +0300

    rev2

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -20,11 +20,11 @@ public class Bot extends TelegramLongPollingBot{
     public static void main(String[] args) {
 
 
-        //НАСТРОЙКИ ПРОКСИ ПРИ ВКЛЮЧЕННОМ ТОРЕ
-//        System.getProperties().put("proxySet", "true");
-//        System.getProperties().put("socksProxyHost", "127.0.0.1");
-//        System.getProperties().put("socksProxyPort", "9150");
-        // ЗАПИХНУТЬ В КОНФИГ
+
+        System.getProperties().put("proxySet", "true");
+        System.getProperties().put("socksProxyHost", "127.0.0.1");
+        System.getProperties().put("socksProxyPort", "9150");
+
 
         ApiContextInitializer.init();
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
